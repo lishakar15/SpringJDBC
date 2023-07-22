b9230caf8565b0580663d809716e8f1922a044db
Lishakar <lishakarj@gmail.com>
2023-07-22 18:40:23 +0530

    Added update and delete functionalities

--- src/main/java/com/additi/Dao/StudentDao.java
+++ src/main/java/com/additi/Dao/StudentDao.java
@@ -4,4 +4,9 @@ import com.additi.Entities.Student;
 
 public interface StudentDao {
     public int insert(Student s);
+
+    public int updateDataById(Student s);
+
+    public int deleteDataById(int id);
+
 }
--- src/main/java/com/additi/Dao/StudentDaoImpl.java
+++ src/main/java/com/additi/Dao/StudentDaoImpl.java
@@ -8,7 +8,20 @@ public class StudentDaoImpl implements StudentDao {
 
     public int insert(Student s) {
         String insertQuery = "insert into student values (?,?,?)";
-        int r = this.jdbcTemplate.update(insertQuery,s.getId(),s.getName(),s.getCity());
+        int r = this.jdbcTemplate.update(insertQuery,s.getId(),s.getCity(),s.getName());
+        return r;
+    }
+
+    public int updateDataById(Student s) {
+        String updateQuery = "update student set name = ?,city =? where id = ?";
+        int r = this.jdbcTemplate.update(updateQuery,s.getName(),s.getCity(),s.getId());
+
+        return r;
+    }
+
+    public int deleteDataById(int id) {
+        String deleteQuery = "delete from student where id =?";
+        int r = this.jdbcTemplate.update(deleteQuery,id);
         return r;
     }
 
